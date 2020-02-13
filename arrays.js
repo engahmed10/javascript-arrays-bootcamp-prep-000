fcadbb860b1d7875d0cf7002a41c67d4debfe925
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 03:14:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = [ "snickers", "hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray( array,element)
 {
   
-  chocolateBars[array] = element;
+  chocolateBars[array].push() = element;
   return chocolateBars
 }
 
