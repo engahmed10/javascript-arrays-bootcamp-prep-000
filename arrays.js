7da84deefc4c68ae4e7b26a8b183182d28bbde5c
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 05:37:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,6 @@ function removeElementFromBeginningOfArray(array)
 }
 function destructivelyRemoveElementFromEndOfArray(array)
 {
-  var elm = array.shift(array.length-1)
+  var elm = array.shift(array.length)
    return elm
 }
